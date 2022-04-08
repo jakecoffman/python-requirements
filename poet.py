e9287dc445f21a21f354d55e8eb1c59e8eeb794f
Jake Coffman <jake@jakecoffman.com>
2022-04-08 14:09:51 -0500

    more assertive

--- poet.py
+++ poet.py
@@ -1,14 +1,22 @@
 from poetry.core.packages.dependency import Dependency
 from poetry.core.semver.version import Version
-from poetry.core.version.requirements import Requirement
+from poetry.core.version.requirements import Requirement, InvalidRequirement
+
+try:
+    Dependency.create_from_pep_508("dummy^1")
+except InvalidRequirement:
+    pass
+else:
+    assert False
 
 d = Dependency("dummy", "^1")
-# d = Dependency.create_from_pep_508("dummy^1") doesn't work
 assert str(d.constraint) == ">=1,<2", d.constraint
 
+# doesn't parse, falls back to *
+assert str(Dependency("dummy", "(>=20.4.3,<20.4.5||>=20.4.7)").constraint) == "*"
+
 d = Dependency.create_from_pep_508("dummy(>=20.4.3,<20.4.5||>=20.4.7)")
-# d = Dependency("dummy", "(>=20.4.3,<20.4.5||>=20.4.7)") this results in * which is a fallback
-print(d.constraint)
+assert str(d.constraint) == ">=20.4.3,<20.4.5 || >=20.4.7", str(d.constraint)
 
 assert d.constraint.allows(Version.parse("20.4.3"))
 assert d.constraint.allows(Version.parse("20.4.4"))
